2125cbcc20c74226a191c070e393317e9e6d90ff
Nerun <39396637+neruneru0419@users.noreply.github.com>
2019-01-27 10:30:43 +0900

    Update koumebot.rb

--- koumebot.rb
+++ koumebot.rb
@@ -6,8 +6,8 @@ conf.token = ENV['SLACK_API_TOKEN']
 end
 client = Slack::Web::Client.new
 loop do
-    if get_contest_plan() then
-        contest_search()
+    if get_contest_plan then
+        contest_search
         if $atflg then
             $atcoder.each do |atlink|        
                 client.chat_postMessage(channel: 'kyopro', text: "今日のAtcoderの情報です\n#{atlink}", as_user: true)
