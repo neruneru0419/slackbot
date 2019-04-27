9cf8415f968415ef4364cc1e61d7d518936945a7
Nerun <39396637+neruneru0419@users.noreply.github.com>
2019-04-27 11:37:19 +0900

    ぽ

--- koumebot.rb
+++ koumebot.rb
@@ -2,20 +2,18 @@ require 'slack-ruby-client'
 require './atcoder'
 
 Slack.configure do |conf|
-conf.token = ENV['SLACK_API_TOKEN']
+    conf.token = ENV['SLACK_API_TOKEN']
 end
 
 client = Slack::Web::Client.new
 
 loop do
-    if get_contest_plan then
+    if get_contest_plan and $atflg then
         contest_search
-        if $atflg then
-            $atcoder.each do |atlink|        
-                client.chat_postMessage(channel: '21_kyopro', text: "今日のAtCoderの情報です\n#{atlink}", as_user: true)
-                $atflg = false
-           　end
-            $atcoder.clear
+        $atcoder.each do |atlink|        
+            client.chat_postMessage(channel: '21_kyopro', text: "今日のAtCoderの情報です\n#{atlink}", as_user: true)
+            $atflg = false
         end
+        $atcoder.clear
     end
 end
